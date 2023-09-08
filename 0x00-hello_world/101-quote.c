7445ecf61fe97250ced962c91fb5ca95f53d5422
Lurmphy <sagbolaoladipupo@gmail.com>
2023-09-07 22:20:11 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - C program that prints a new line,followed by a  standard error
  * Return: 1
  */
- int main(void)
+int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (0);
