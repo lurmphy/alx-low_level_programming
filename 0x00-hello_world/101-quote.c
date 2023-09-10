ebad8f0551ea02ab7a480f5d5ae457041fae0c6d
Lurmphy <sagbolaoladipupo@gmail.com>
2023-09-10 16:23:58 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - C program that prints a new line,followed by a  standard error
+ * main - program that prints a new line
  * Return: 1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (0);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
